8775221c3fe36acdb9d5742c708fc692aaa17eed
jair-pereira <jair.pereira@aluno.ufabc.edu.br>
2019-09-27 05:29:03 +0900

    sync

--- testing/call_frk.py
+++ testing/call_frk.py
@@ -2,12 +2,12 @@ import numpy as np
 import cocoex, cocopp
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from solvers import f1705
+from solvers import f1702
 
 nfe = 1e+6
-suite = cocoex.Suite("bbob", "", "function_indices:17 dimensions:5 instance_indices:2")
+suite = cocoex.Suite("bbob", "", "function_indices:17 dimensions:2 instance_indices:2")
 
 for reps in range(10):
     for problem in suite:
-        sol = f1705(0, problem, (problem.lower_bounds[0], problem.upper_bounds[0]), problem.dimension, nfe)
+        sol = f1702(0, problem, (problem.lower_bounds[0], problem.upper_bounds[0]), problem.dimension, nfe)
         print(sol.best.getFitness())
