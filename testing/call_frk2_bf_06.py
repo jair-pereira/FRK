c2f32786f847cfc164098169eef4e1a1d4ddde56
jair-pereira <jair.pereira@aluno.ufabc.edu.br>
2020-01-26 10:01:59 +0900

    sync t

--- testing/call_frk2_bf_06.py
+++ testing/call_frk2_bf_06.py
@@ -2,16 +2,17 @@ import numpy as np
 import cocoex, cocopp
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from frk2_2f import *
+from frk2_bf import *
 
-suite = cocoex.Suite("bbob", "", "function_indices:01,06,15,21 dimensions:10 instance_indices:1-15")
-observer = cocoex.Observer("bbob", "result_folder: " + "FRK22f_all")
+suite = cocoex.Suite("bbob", "", "function_indices:06 dimensions:10 instance_indices:1-15")
+observer = cocoex.Observer("bbob", "result_folder: " + "frk2bf_01")
 
 nfe = 1e+6
 repetitions = 10
 
-solver_list = iter([frk2_2f01]*15 + [frk2_2f06]*15 + [frk2_2f15]*15 + [frk2_2f21]*15)
-# solver_list = iter([frk2_2f01]*1 + [frk2_2f06]*1 + [frk2_2f15]*1 + [frk2_2f21]*1)
+# solver_list = iter([frk2_bf01]*15 + [frk2_bf06]*15 + [frk2_bf15]*15 + [frk2_bf21]*15)
+# solver_list = iter([frk2_bf01]*1 + [frk2_bf06]*1 + [frk2_bf15]*1 + [frk2_bf21]*1)
+solver_list = iter([frk2_bf06]*15)
 
 
 for problem in suite:
@@ -24,6 +25,10 @@ for problem in suite:
         if not problem.final_target_hit:
             print(i, end=" ")
             S = solver(problem, (problem.lower_bounds[0], problem.upper_bounds[0]), problem.dimension, nfe)
+
+            fw = open("frk2_bf06_"+str(problem.id)+"_r"+i+".pkl", "wb")
+            pickle.dump(S, fw)
+            fw.close()
 
     print()
 
